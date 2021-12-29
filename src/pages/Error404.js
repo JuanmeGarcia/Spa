3b640d7826b63837c529bae2b992ab846d3c83ab
Juanme <jmgcasd@gmail.com>
2021-12-29 19:38:28 -0300

    curso finalizado

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 =  ()=>{
+    const error = /* html */`
+        <div class="error404">
+            <h2>Error 404</h2>
+        </div>
+    `
+
+    return error
+}
+
+export default Error404
---
+++ src/templates/Header.js
@@ -0,0 +1,22 @@
+const Header = () =>{
+    const header = /* html */`
+        <div class="header-main">
+            <div class="header-logo">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+
+            <div class="header-nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </div>
+        `
+    return header
+}
+
+export default Header
